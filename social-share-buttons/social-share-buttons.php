ee61dbe4788a60fc26feab9ad61fa2fce7cb0e13
Narek Malkhasyan <narek.mal@gmail.com>
2017-07-06 15:52:11 +0400

    Added plugin folder and main file

---
+++ social-share-buttons/social-share-buttons.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Social Share Buttons
+Description: Adds social share buttons to posts and pages.
+Author: Narek Malkhasyan
+Text Domain: social-share-buttons
+Version: 0.0.1
+*/
+
+?>
